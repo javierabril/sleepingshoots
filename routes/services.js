16d4ddf16e665357cd795006d71c77483152286f
Javi <javierabril84@gmail.com>
2018-03-05 17:37:42 +0100

    Select en DB

--- routes/services.js
+++ routes/services.js
@@ -1,5 +1,7 @@
 module.exports = function (app) {
 
+    var db = require('../dbazure');
+
     app.post('/register', function (req, res) {
 
         var sha = require('sha256');
@@ -11,9 +13,47 @@ module.exports = function (app) {
             password: sha(req.body.password)
         };
 
-        var result = { ok: true };
+        var userEncontrado = buscaUser();
+
+        var result = {
+            ok: true,
+            numFilas: userEncontrado
+        };
         res.send(user);
 
     });
 
+
+    var Request = require('tedious').Request;
+    var TYPES = require('tedious').TYPES;
+
+    function buscaUser(userName) {
+        request = new Request("SELECT * FROM Users Where nombre like " + userName + ";", function (err) {
+            if (err) {
+                console.log(err);
+            }
+        });
+        var result = "";
+        request.on('row', function (columns) {
+            columns.forEach(function (column) {
+                if (column.value === null) {
+                    console.log('NULL');
+                } else {
+                    result += column.value + " ";
+                }
+            });
+            console.log(result);
+            result = "";
+        });
+
+        var numFilas;       
+
+        request.on('done', function (rowCount, more) {
+            console.log(rowCount + ' rows returned');
+            numFilas = rowCount;
+        });
+        connection.execSql(request);
+
+        return numFilas;
+    }  
 }

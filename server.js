e0b2a92490281705cb15f81b5d461b6730cef637
Javi <javierabril84@gmail.com>
2018-03-05 16:18:16 +0100

    update

--- server.js
+++ server.js
@@ -181,11 +181,12 @@ function addUser(source, sourceUser) {
 
 */
 
+const secretWord = "miclave";
 
 var app = express();
 app.configure(function () {
     app.set('port', process.env.PORT || 3000);
-    app.set('views', __dirname + '/views');
+    /*app.set('views', __dirname + '/views');
     app.set('view engine', 'jade');
     app.use(express.favicon());
     app.use(express.logger('dev'));
@@ -196,7 +197,7 @@ app.configure(function () {
     app.use(everyauth.middleware(app));
     app.use(app.router);
     app.use(require('less-middleware')({ src: __dirname + '/public' }));
-    app.use(express.static(path.join(__dirname, 'public')));
+    app.use(express.static(path.join(__dirname, 'public')));*/
     app.use(bodyParser.json());
 
 });
@@ -269,7 +270,7 @@ var server = http.createServer(app);
 **/
 
 // socket.io configuration
-var buffer = [];
+/*var buffer = [];
 var io = require('socket.io').listen(server);
 
 
@@ -300,7 +301,7 @@ io.sockets.on('connection', function (socket) {
     })
 });
 
-
+*/
 /**
 * RUN
 * -------------------------------------------------------------------------------------------------
